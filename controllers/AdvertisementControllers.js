177772652794c51822ad746f47a17054934900f4
sasha <sasha.v.bes@gmail.com>
2023-08-25 18:16:25 +0300

    upgrate

--- controllers/AdvertisementControllers.js
+++ controllers/AdvertisementControllers.js
@@ -1,4 +1,4 @@
-const { Advertisement } = require('../models/index')
+const { Advertisement, User, Category } = require('../models/index')
 const jwt = require("jsonwebtoken");
 
 const AdvertisementController = {
@@ -9,18 +9,41 @@ const AdvertisementController = {
     },
 
     async getAdvertisement(req, res) {
-        // const token = req.headers.authorization.replace('Bearer ', '')
-        // try {
-        //     const jwtToken = jwt.verify(token, 'fqugwqgfuiewfgefugieho')
-        //     console.log(jwtToken);
-        // } catch (err) { 
-        //     return res.status(401).json({message:'не авторизован'})
-        // }
+        const withCheck = req.query?.withCheck
 
+        if (withCheck === 'true') {
+            const token = req.headers?.authorization
+            const splitToken = token.split(' ')[1]
+            try {
+                const userInfo = jwt.verify(splitToken, 'fqugwqgfuiewfgefugieho')
+
+                const advertisement = await Advertisement.findOne({
+                    where: { id: req.params.id },
+                    include: [
+                        { model: Category }
+                    ]
+                })
+
+                if (!advertisement) { return res.status(422).json({ message: 'объявление не найдено' }) }
+
+                if (advertisement.userId !== userInfo.id) { return res.status(422).json({ message: 'объявление не соответствует пользователю' }) }
+
+                return res.status(200).json(advertisement)
+
+            } catch (err) {
+                return res.status(401).json({ message: 'пользователь не авторизован' })
+            }
+
+        }
 
         const advertisement = await Advertisement.findOne({
-            where: { id: req.params.id }
+            where: { id: req.params.id },
+            include: [
+                { model: Category }
+            ]
         })
+        if (advertisement === null) { return res.status(400).json({ message: 'объявление не найдено' }) }
+
         return res.status(200).json(advertisement)
     },
 
@@ -36,15 +59,11 @@ const AdvertisementController = {
     async createAd(req, res) {
         const name = req.body.name
         if (name.length < 2) { return res.status(400).json({ message: "название меньше 2-x символов" }) }
-
         if (name.length > 40) { return res.status(400).json({ message: "название больше 40 символов" }) }
-
-        const category = req.body.category
+        const category = req.body.categoryId
         if (!category) { return res.status(400).json({ message: "категория не выбрана" }) }
-
         const description = req.body.description
         if (description.length < 20) { return res.status(400).json({ message: "описание меньше 20-ти символов" }) }
-
         const image = req.body.image
         if (!image) { return res.status(400).json({ message: "фотографии не выбраны" }) }
 
@@ -77,11 +96,65 @@ const AdvertisementController = {
                 return res.status(401).json({ message: 'пользователь не авториезован' })
             }
         }
-
-
-
-
     },
+    async editAd(req, res) {
+        const token = req.headers.authorization
+        if (!token) { return res.status(401).json({ message: 'вы не авторизованы' }) }
+
+        try {
+            const userInfo = jwt.verify(token.split(' ')[1], 'fqugwqgfuiewfgefugieho')
+
+            try {
+
+                const editedAdvertisement = await Advertisement.update(
+                    {
+                        name: req.body.name,
+                        description: req.body.description,
+                        image: req.body.image
+                    },
+                    {
+                        where: {
+                            id: req.params.id
+                        }
+                    }
+                )
+                console.log(req.body);
+                console.log(editedAdvertisement);
+                return res.status(200).json('')
+            } catch (error) {
+                return res.status(400).json({ message: "что-то пошло не так" })
+            }
+
+        } catch (error) {
+            return res.status(401).json({ message: 'вы не авторизованы' })
+        }
+    },
+    async deleteAdvertisement(req, res) {
+        const token = req.headers.authorization
+        if (!token) { return res.status(401).json({ message: 'вы не авторизованы' }) }
+
+        // console.log(req.params.id);
+
+        // return res.status(200).json({ message: '' })
+        try {
+
+            try {
+                const deleteAdvertisement = await Advertisement.destroy(
+                    {
+                        where: {
+                            id: req.params.id
+                        }
+                    }
+                )
+                return res.status(200).json(deleteAdvertisement)
+            } catch (error) {
+                return res.status(400).json({ message: "что-то пошло не так" })
+            }
+
+        } catch (error) {
+            return res.status(401).json({ message: 'вы не авторизованы' })
+        }
+    }
 }
 
 module.exports = AdvertisementController
--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,9 +1,10 @@
-const { User } = require("../models/index");
+const { User, Advertisement, Category } = require("../models/index");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const cookie = require("js-cookie")
+const cookie = require("js-cookie");
 
 const UserController = {
+
   async registration(req, res) {
     console.log(req.body);
     const login = req.body.login;
@@ -27,7 +28,6 @@ const UserController = {
   },
 
   async authentication(req, res) {
-
     const user = await User.findOne({
       where: { login: req.body.login },
     });
@@ -42,22 +42,138 @@ const UserController = {
 
       if (!isPasswordRight) { return res.status(400).json({ message: "пароль неверный" }) }
 
-      const userToken = jwt.sign({ id: user.id }, "fqugwqgfuiewfgefugieho", {
-        expiresIn: "15m",
-      });
-      // console.log(req.body.);
-
-      // if () {
-      // }
-
+      const userToken = jwt.sign(
+        { id: user.id },
+        "fqugwqgfuiewfgefugieho",
+        { expiresIn: "55m", }
+      );
       cookie.set('userToken', userToken, { expires: 1 })
 
       return res.status(200).json({ id: user.id, token: userToken });
 
-    } catch (error) {
+    }
+    catch (error) {
       console.log(error);
     }
   },
+
+  async getUserAdvertisements(req, res) {
+    const token = req.headers.authorization
+    if (!token) { return res.status(401).json({ message: "пользователь не авторизован" }) }
+
+    const splitToken = token.split(' ')[1]
+    try {
+      const userInfo = jwt.verify(splitToken, 'fqugwqgfuiewfgefugieho')
+      const advertisements = await Advertisement.findAll({
+        where: {
+          userId: userInfo.id
+        },
+        include: [
+          {
+            model: Category,
+            // include: [{
+            //   model:
+            // }]
+          }
+        ]
+      })
+      return res.status(200).json(advertisements)
+    }
+    catch (err) {
+      console.log(err);
+      return res.status(401).json({ message: "пользователь не авторизован" })
+    }
+  },
+
+  async getUserInfo(req, res) {
+    const token = req.headers.authorization
+    if (!token) { return res.status(401).json({ message: "пользователь не авторизован" }) }
+
+    const splitToken = token.split(' ')[1]
+
+    try {
+      const userInfo = jwt.verify(splitToken, 'fqugwqgfuiewfgefugieho')
+      const user = await User.findOne({
+        where: {
+          id: userInfo.id
+        }
+      })
+
+      const formatedUser = {
+        id: user.id,
+        login: user.login,
+      }
+
+      return res.status(200).json(formatedUser)
+    }
+    catch (err) {
+      return res.status(401).json({ message: 'вы не авторизованы' })
+    }
+  },
+  async changePassword(req, res) {
+
+    const token = req.headers.authorization
+    if (!token) { return res.status(401).json({ message: "пользователь не авторизован" }) }
+
+    const splitToken = token.split(' ')[1]
+
+    try {
+      const userInfo = jwt.verify(splitToken, 'fqugwqgfuiewfgefugieho')
+      const user = await User.findOne({
+        where: {
+          id: userInfo.id
+        }
+      })
+
+      if (!user) { return res.status(400).json({ message: 'такого пользователя нет' }) }
+
+      const isPasswordRight = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
+
+      if (!isPasswordRight) { return res.status(422).json({ message: 'старый пароль - неверный' }) }
+      console.log(userInfo.id);
+
+      try {
+        const hashedPassword = await bcrypt.hash(req.body.newPassword, 2);
+
+        await User.update(
+          {
+            password: hashedPassword
+          },
+          {
+            where: {
+              id: userInfo.id
+            }
+          }
+        )
+
+        return res.status(200).json('пароль изменен')
+      } catch (err) {
+        return res.status(400).json({ message: 'dddddd' })
+      }
+
+    } catch (err) {
+      return res.status(401).json({ message: 'вы не авторизованы' })
+    }
+  },
+  async checkAuth(req, res) {
+    const token = req.headers?.authorization
+    if (!token) { return res.status(401).json({ message: 'unauthorized' }) }
+
+    const splitToken = token.split(' ')[1]
+    
+    try {
+      jwt.verify(splitToken, 'fqugwqgfuiewfgefugieho')
+
+      return res.status(200).json({ message: '' })
+    } catch (error) {
+      return res.status(401).json({ message: 'unauthorized' })
+    }
+
+
+  }
 };
 
 module.exports = UserController;
